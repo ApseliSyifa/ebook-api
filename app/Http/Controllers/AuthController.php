3771062b5dbe0c6ef57809a336a7a5b4285dd40c
ApseliSyifa <69224454+ApseliSyifa@users.noreply.github.com>
2021-10-11 15:15:58 +0700

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,10 +9,10 @@ class AuthController extends Controller
     public function index()
     {
         return [
-            "NIS" => 3103119032,
-            "Name" => "Atina Nora Haya",
+            "NIS" => 3103119026,
+            "Name" => "Apseli Syifa",
             "Gender" => "Perempuan",
-            "Phone" => 6281228467337,
+            "Phone" => 081229458085,
             "Class" => "XII RPL1"
         ];
     }
